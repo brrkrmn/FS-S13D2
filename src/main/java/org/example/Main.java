4a7c5a507c82a9e323b96a4391beadb49d6d539a
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-03-04 12:36:47 +0300

    Create methods for palindrome, perfect number and number to words

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int value) {
+        String[] chars = String.valueOf(value).replace("-", "").split("");
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (!chars[i].equals(chars[chars.length - 1 - i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPerfectNumber(int value) {
+        if (value < 0) return false;
+
+        int total = 0;
+
+        for (int i = 1; i < value; i++) {
+            if (value % i == 0) {
+                total += i;
+            }
+        }
+
+        return total == value;
+    }
+
+    public static String numberToWords(int value) {
+        if (value < 0) return "Invalid Value";
+
+        String[] nums = String.valueOf(value).split("");
+        String[] words = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        String result = "";
+
+        for (String num: nums) {
+            result = result.concat(words[Integer.parseInt(num)]).concat(" ");
+        }
+
+        return result.trim();
+    }
 }
